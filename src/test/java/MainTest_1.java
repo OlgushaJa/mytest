51a0a11d2b903820bdec8e1956beb02edbaccb52
OlgushaJa <Olhamysl.7@gmail.com>
2021-10-22 00:21:15 +0300

    First commit

---
+++ src/test/java/MainTest_1.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest_1 {
+
+    @Test
+    public void testPrice() {
+        System.setProperty("webdriver.chrome.driver", "c:/Users/Оля/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        try {
+
+            driver.manage().window().maximize();
+            driver.get("https://umedlab.com.ua/ru/glavnaya");
+
+            WebElement searchButton = driver.findElement(By.className("get_button"));
+
+            searchButton.click();
+
+            String expectedUrl = "https://umedlab.com.ua/ru/prajs/";
+            String actualUrl = driver.getCurrentUrl();
+
+            Assert.assertEquals(actualUrl, expectedUrl);
+        }finally {
+            driver.quit();
+        }
+
+    }
+}
